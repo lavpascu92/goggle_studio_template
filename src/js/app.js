989d47a8fca9783c4a10ce2ad782e8ddf1a4dd91
Lavinia P <pascu.lavinianicoleta@gmail.com>
2020-08-19 18:10:27 +0300

    Added more animations and made it to 85% responsiveness

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,15 @@
-AOS.init();
+$(document).ready(function(){
+    // AOS plugin initialization
+    AOS.init();   
+    // Hero background image slight move on mousemove
+    $('.hero').mousemove(function(el){
+        var moveX = (el.pageX * 1 / 150);
+        var moveY = (el.pageY * 1 / 150);
+        $(this).css('background-position', moveX + "px " + moveY + "px");
+    });
+    // Fade out hero background image on scroll down
+    $(window).scroll(function(){
+        $(".hero").css("opacity", 1 - $(window).scrollTop() / ($('.hero').height() / 1.5));
+    });
+    
+});
